b97f1ad54a59981abe899a0d96b765b639ed998b
Vo Quang Chien <chiendien2006@gmail.com>
2023-02-25 20:29:00 +0700

    clock.js

---
+++ clock.js
@@ -0,0 +1,14 @@
+const hr = document.getElementById("hr");
+const min = document.getElementById("min");
+const sec = document.getElementById("sec");
+
+setInterval(() => {
+  const date = new Date();
+  const hh = date.getHours() * 30;
+  const mm = date.getMinutes() * 6;
+  const ss = date.getSeconds() * 6;
+
+  hr.style.transform = `rotateZ(${hh}deg)`;
+  min.style.transform = `rotateZ(${mm}deg)`;
+  sec.style.transform = `rotateZ(${ss}deg)`;
+});
