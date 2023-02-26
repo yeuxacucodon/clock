ce56532c6aa2db78fd64074d5daafcde978681de
Vo Quang Chien <chiendien2006@gmail.com>
2023-02-26 09:18:58 +0700

    format

--- clock.js
+++ clock.js
@@ -1,26 +1,26 @@
-const hr = document.getElementById("hr")
-const min = document.getElementById("min")
-const sec = document.getElementById("sec")
+const hr = document.getElementById("hr");
+const min = document.getElementById("min");
+const sec = document.getElementById("sec");
 
 rotation = (target, val) => {
-  target.style.transform = `rotate(${val}deg)`
-}
+  target.style.transform = `rotate(${val}deg)`;
+};
 
 clock = () => {
-  let date = new Date()
-  let hh = (date.getHours() % 12) + date.getMinutes() / 59
-  let mm = date.getMinutes()
-  let ss = date.getSeconds()
+  let date = new Date();
+  let hh = (date.getHours() % 12) + date.getMinutes() / 59;
+  let mm = date.getMinutes();
+  let ss = date.getSeconds();
 
-  hh *= 30
-  mm *= 6
-  ss *= 6
+  hh *= 30;
+  mm *= 6;
+  ss *= 6;
 
-  rotation(hr, hh)
-  rotation(min, mm)
-  rotation(sec, ss)
+  rotation(hr, hh);
+  rotation(min, mm);
+  rotation(sec, ss);
 
-  setTimeout(clock, 500)
-}
+  setTimeout(clock, 500);
+};
 
-window.onload = clock()
+window.onload = clock();
